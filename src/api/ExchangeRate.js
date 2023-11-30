424d5d926f492727cc117337a0b74ae0028bbbef
jugpark <hjjk0721@gmail.com>
2023-11-30 13:28:23 +0900

    fix: api call error

--- src/api/ExchangeRate.js
+++ src/api/ExchangeRate.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+const PROXY = window.location.hostname === 'localhost' ? '/api/site/program/financial/exchangeJSON' : '/proxy/';
+
 export async function getExchangeRate () {
     const authkey = "9NIQpTa23qxxlYsKcOsfB2IF2zNngokx";
     const searchdate = getToday();
     const data = "AP01";
-    const baseURL = `/api/site/program/financial/exchangeJSON?authkey=${authkey}&searchdate=${searchdate}&data=${data}`;
+    const baseURL = `${PROXY}?authkey=${authkey}&searchdate=${searchdate}&data=${data}`;
     let result;
     await axios.get(baseURL).then(res => {
         result = res.data
